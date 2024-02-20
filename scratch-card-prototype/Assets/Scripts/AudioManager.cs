db63b91dbdb9dbdf435febe16ee94be8eb35a66b
Blaer <blaerzhang@gmail.com>
2024-02-20 15:15:21 -0500

    Unfinished IconBase

---
+++ scratch-card-prototype/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+namespace Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public void PlaySound(AudioClip audioClip)
+        {
+            GameObject tempAudio = new GameObject("Temp Audio");
+            AudioSource tempAudioSource = tempAudio.AddComponent<AudioSource>();
+
+            tempAudioSource.playOnAwake = false;
+            tempAudioSource.clip = audioClip;
+            tempAudioSource.Play();
+            StartCoroutine(DestroyTempAudio(tempAudio));
+        }
+
+        private IEnumerator DestroyTempAudio(GameObject tempAudio)
+        {
+            float clipLength = tempAudio.GetComponent<AudioSource>().clip.length;
+            yield return new WaitForSeconds(clipLength + 1f);
+            Destroy(tempAudio);
+        }
+    }
+}
---
+++ scratch-card-prototype/Assets/Scripts/icons/IconBase.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Managers;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class IconBase : MonoBehaviour
+{
+    [Header("Feedbacks")]
+    public bool playFeedbacks = true;
+    public List<AudioClip> feedbackSoundList;
+    
+    [Header("Icon")]
+    public bool fullScratchToReveal = false;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    protected void OnReveal()
+    {
+        //TODO:automatic scratch animation
+        
+        if (playFeedbacks)
+        {
+            PlayFeedbackAnimation();
+            if (feedbackSoundList.Count > 0)
+            {
+                AudioManager.instance.PlaySound(feedbackSoundList[Random.Range(0, feedbackSoundList.Count)]);
+            }
+        }
+        
+        ApplyEffect();
+    }
+
+    protected void PlayFeedbackAnimation()
+    {
+        
+    }
+
+    protected void ApplyEffect()
+    {
+        
+    }
+}
